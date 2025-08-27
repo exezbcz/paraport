2c68494c2fb6138816a9ffb640aa6b31cab30dbe
hassnian <44554284+hassnian@users.noreply.github.com>
2025-08-27 10:43:06 +0500

    ref(core): chain utils to chain.ts

--- packages/core/src/utils/chains.ts
+++ packages/core/src/utils/chains.ts
@@ -1,5 +1,11 @@
 import {
+	ASSET_CHAINS_MAP,
+	type Asset,
+	CHAINS,
+	CHAIN_NAMES,
 	type Chain,
+	type ChainProperties,
+	ENDPOINT_MAP,
 	existentialDeposit,
 	teleportExistentialDeposit,
 } from '@paraport/static'
@@ -10,4 +16,28 @@ export const edOf = (chain: Chain): bigint => {
 
 export const teleportEdOf = (chain: Chain): bigint => {
 	return BigInt(teleportExistentialDeposit[chain])
+}
+
+export const chainPropListOf = (chain: Chain): ChainProperties => {
+	return CHAINS[chain]
+}
+
+export const ss58Of = (chain: Chain): number => {
+	return chainPropListOf(chain).ss58Format
+}
+
+export const decimalsOf = (chain: Chain): number => {
+	return chainPropListOf(chain).tokenDecimals
+}
+
+export const getChainsOfAsset = (asset: Asset): Chain[] => {
+	return ASSET_CHAINS_MAP[asset] || []
+}
+
+export const endpointOf = (chain: Chain): string => {
+	return ENDPOINT_MAP[chain]
+}
+
+export const getChainName = (chain: Chain): string => {
+	return CHAIN_NAMES[chain]
 }
--- packages/core/src/utils/index.ts
+++ packages/core/src/utils/index.ts
@@ -1,33 +1,5 @@
-import { formatAddress } from '@/utils/account'
-import { transferableBalanceOf } from '@/utils/balance'
-import { edOf, teleportEdOf } from '@/utils/chains'
-import {
-	ASSET_CHAINS_MAP,
-	type Asset,
-	CHAINS,
-	type Chain,
-	type ChainProperties,
-	ENDPOINT_MAP,
-} from '@paraport/static'
-
-export const chainPropListOf = (chain: Chain): ChainProperties => {
-	return CHAINS[chain]
-}
-
-export const ss58Of = (chain: Chain): number => {
-	return chainPropListOf(chain).ss58Format
-}
-
-export const decimalsOf = (chain: Chain): number => {
-	return chainPropListOf(chain).tokenDecimals
-}
-
-export const getChainsOfAsset = (asset: Asset): Chain[] => {
-	return ASSET_CHAINS_MAP[asset] || []
-}
-
-export const endpointOf = (chain: Chain): string => {
-	return ENDPOINT_MAP[chain]
-}
-
-export { formatAddress, edOf, teleportEdOf, transferableBalanceOf }
+export * from '@/utils/account'
+export * from '@/utils/balance'
+export * from '@/utils/chains'
+export * from '@/utils/number'
+export * from '@/utils/tx'
