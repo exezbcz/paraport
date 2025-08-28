66418ec589018dc38ce660aa5321c84f8dae75cc
hassnian <44554284+hassnian@users.noreply.github.com>
2025-08-28 15:26:31 +0500

    ref(playground): remove unused dependencies and simplify code
    Remove unused dependencies `@paraport/static`, `@paraport/core`, and `@polkadot/extension-dapp` to streamline the project. Simplify the main.js file by removing unnecessary imports and hardcoding chain and asset values directly in the configuration

--- packages/playground/main.js
+++ packages/playground/main.js
@@ -1,24 +1,18 @@
-import { Asset, Chain } from '@paraport/static'
-import { web3Enable } from '@polkadot/extension-dapp'
-
 import '@paraport/ui/style'
 import * as paraport from '@paraport/ui'
 
-const DAPP_NAME = 'Kodadot'
 const USER_ADDRESS = 'CykZSc3szpVd95PmmJ45wE4ez7Vj3xkhRFS9H4U1WdrkaFY'
 const AMOUNT = '600000000000'
 
 const main = async () => {
-  await web3Enable(DAPP_NAME)
-
   paraport.init({
     integratedTargetId: 'root',
     label: 'Confirm',
     autoteleport: {
   		address: USER_ADDRESS,
   		amount: AMOUNT,
-  		chain: Chain.AssetHubKusama,
-  		asset: Asset.KSM,
+  		chain: 'AssetHubKusama',
+  		asset: 'KSM',
   	},
     logLevel: 'DEBUG',
     onSubmit: (autoteleport) => {
