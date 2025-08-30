82ef12b14b52a6143980b32343da54945f87bd25
hassnian <44554284+hassnian@users.noreply.github.com>
2025-08-30 15:09:53 +0500

    feat(IntegratedLoading): add spinner icon to loading component

--- packages/sdk/src/components/integrated/IntegratedLoading.vue
+++ packages/sdk/src/components/integrated/IntegratedLoading.vue
@@ -1,7 +1,10 @@
 <template>
    <Container top-classes="bg-surface-grey">
         <template #top>
-            <div class="inline-flex gap-3 items-center justify-center">
+            <div class="inline-flex gap-3 items-center justify-center w-full text-secondary">
+                <div class="animate-spin">
+                    <LoaderCircleIcon :size="20" />
+                </div>
                 <span class="text-[16px] capitalize font-medium">
                     {{ t('autoteleport.checking' )}}
                 </span>
@@ -21,6 +24,7 @@
 import FullLogo from '@/components/common/FullLogo.vue'
 import { useI18n } from 'vue-i18n'
 import Container from './Container.vue'
+import { LoaderCircleIcon } from 'lucide-vue-next'
 
 const { t } = useI18n()
 </script>
