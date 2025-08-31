13c8e8f0aeec71f48134308d5b39eb0c71a63b3e
hassnian <44554284+hassnian@users.noreply.github.com>
2025-08-31 14:41:31 +0500

    build: update SDK version and increase dialog z-index
    Update the SDK version from 0.1.0-alpha.1 to 0.1.0-alpha.2 to reflect changes. Increase the z-index of dialog components to ensure they appear above other elements in the UI.

--- packages/sdk/src/components/ui/dialog/DialogContent.vue
+++ packages/sdk/src/components/ui/dialog/DialogContent.vue
@@ -34,14 +34,14 @@ const forwarded = useForwardPropsEmits(delegatedProps, emits)
   <DialogPortal>
     <DialogOverlay
       v-if="overlay"
-      class="fixed inset-0 z-50 bg-black/80 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0"
+      class="fixed inset-0 z-[999] bg-black/80 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0"
     />
 
     <DialogContent
       v-bind="forwarded"
       :class="
         cn(
-          'fixed z-50 grid w-full max-w-xs gap-4 border bg-background p-5 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95',
+          'fixed z-[999] grid w-full max-w-xs gap-4 border bg-background p-5 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95',
           !position && 'left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%]',
           props.class
         )"
