20ac71db8e8f039e47195cd3814c2b71cfa95f21
hassnian <44554284+hassnian@users.noreply.github.com>
2025-10-12 16:13:00 +0500

    ref(static): remove teleportExistentialDeposit

--- packages/static/src/chains.ts
+++ packages/static/src/chains.ts
@@ -48,24 +48,11 @@ export const CHAINS: Config<ChainProperties> = {
 	),
 }
 
-// DEV: note that ED is different from the on-chain ED to prevent weird edge cases of XCM
-export const teleportExistentialDeposit: Record<Chain, number> = {
-	[Chains.Polkadot]: 15000000000,
-	[Chains.AssetHubPolkadot]: 5000000000,
-	[Chains.Kusama]: 666666666,
-	[Chains.AssetHubKusama]: 666666666,
-	[Chains.Hydration]: 666666666,
-}
-
+// TODO: remove
 export const existentialDeposit: Record<Chain, number> = {
 	[Chains.Polkadot]: 1e10,
 	[Chains.AssetHubPolkadot]: 1e8,
 	[Chains.Kusama]: 333333333,
 	[Chains.AssetHubKusama]: 333333333,
-	[Chains.Hydration]: 333333333,
-}
-
-export const ASSET_CHAINS_MAP: Partial<Record<Asset, Chain[]>> = {
-	[Assets.DOT]: [Chains.Polkadot, Chains.AssetHubPolkadot, Chains.Hydration],
-	[Assets.KSM]: [Chains.Kusama, Chains.AssetHubKusama],
+	[Chains.Hydration]: 1000000000000,
 }
